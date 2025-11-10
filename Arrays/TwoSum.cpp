9309e1c6aca1b4c3fbfdabd7f1c72394d78a7099
KartikVerma39 <kartikverma1074@gmail.com>
2025-11-10 12:23:19 +0530

    array_questions

---
+++ Arrays/TwoSum.cpp
@@ -0,0 +1,18 @@
+// Two Sum
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>arr = {2,7,11,15};
+    int target = 9;
+    unordered_map<int,int>mp;
+    for(int i = 0 ; i < arr.size() ; i++){
+        int current = arr[i];
+        int needed = target - current;
+
+        if(mp.find(needed) != mp.end()){
+            cout<<mp[needed]<<" "<<i;
+        }
+        mp[current] = i;
+    }
+}
