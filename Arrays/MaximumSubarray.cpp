17e3cbd5650925a950179db06704d7e8f63edd77
KartikVerma39 <kartikverma1074@gmail.com>
2025-11-11 12:49:50 +0530

    array_questions

---
+++ Arrays/MaximumSubarray.cpp
@@ -0,0 +1,19 @@
+//  Maximum Subarray
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>arr = {-2,1,-3,4,-1,2,1,-5,4};
+    int maxSum = INT8_MIN;
+    int currSum = 0;
+    for(int i = 0 ; i < arr.size() ; i++){
+        currSum += arr[i];
+        maxSum = max(maxSum,currSum);
+        if(currSum < 0){
+            currSum = 0;
+        }
+    }
+    cout<<maxSum;
+}
