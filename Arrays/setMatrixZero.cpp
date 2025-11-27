b0ac0abd103da8dd0966219954c6884088b5f4c2
KartikVerma39 <kartikverma1074@gmail.com>
2025-11-27 10:10:21 +0530

    array_dsa

---
+++ Arrays/setMatrixZero.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<vector<int>>arr = {{1,1,1},{1,0,1},{1,1,1}};
+    int m = arr.size();
+    int n = arr[0].size();
+
+    int col0 = 1;
+
+    for(int i = 0 ; i < m ; i++){
+        for(int j = 0 ; j < n ; j++){
+            if(arr[i][j] == 0){
+                arr[i][0] = 0;
+                if(j!=0){
+                    arr[0][j] = 0;
+                }
+                else{
+                    col0 = 0;
+                }
+            }
+        }
+    }
+
+
+    for(int i = 1 ; i<m ; i++){
+        for(int j = 1 ; j<n ; j++){
+            if(arr[i][j] != 0){
+                if(arr[i][0] == 0 || arr[0][j] == 0){
+                    arr[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    if(arr[0][0] == 0){
+        for(int i = 0 ; i<m ; i++){
+            arr[0][i] = 0;
+        }
+    }
+
+
+    if(col0 == 0){
+        for(int i = 0 ; i<n ; i++){
+            arr[i][0] = 0;
+        }
+    }
+
+
+
+    for(int i = 0 ; i < m ; i++){
+        for(int j = 0 ; j < n ; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
