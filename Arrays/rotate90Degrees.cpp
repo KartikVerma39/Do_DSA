adb49d80c4b8d30a542bc72fc9d8336c5f2dbefd
KartikVerma39 <kartikverma1074@gmail.com>
2025-11-27 10:39:48 +0530

    array_dsa

---
+++ Arrays/rotate90Degrees.cpp
@@ -0,0 +1,29 @@
+// 48. Rotate Image
+
+#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<vector<int>>arr = {{1,2,3},{4,5,6},{7,8,9}};
+
+    int n = arr.size();
+
+    for(int i = 0 ; i < n-1 ; i++){
+        for(int j = i+1 ; j < n ; j++){
+            swap(arr[i][j] , arr[j][i]);
+        }
+    }
+
+    for(int i = 0 ; i < n ; i++){
+        reverse(arr[i].begin() , arr[i].end());
+    }
+
+    for(int i = 0 ; i < n ; i++){
+        for(int j  = 0 ; j < n ; j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
