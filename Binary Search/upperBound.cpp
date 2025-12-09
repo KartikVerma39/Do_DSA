d75bd3260ecafd4f8fc48a2a72e954b51eaaf7f8
KartikVerma39 <kartikverma1074@gmail.com>
2025-12-09 11:01:45 +0530

    BinarySearch_DSA

---
+++ Binary Search/upperBound.cpp
@@ -0,0 +1,27 @@
+// Implement Upper Bound
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>arr = {2, 3, 7, 10, 11, 11, 25};
+    int target = 9;
+    int n = arr.size();
+    int low = 0;
+    int high = n - 1;
+    int ans = n;
+
+    while(low <= high){
+        int mid = low + (high - low)/2;
+
+        if(arr[mid] > target){
+            ans = mid;
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    cout<<ans;
+}
