39c787998cd8ead6b89fb7c5ce82fa09446ff574
KartikVerma39 <kartikverma1074@gmail.com>
2025-12-09 12:20:33 +0530

    BinarySearch_DSA

---
+++ Binary Search/firstAndLastOccurence.cpp
@@ -0,0 +1,49 @@
+// 34. Find First and Last Position of Element in Sorted Array
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>arr = {5,7,7,8,8,10};
+    int target = 7;
+    int n = arr.size();
+    int low = 0;
+    int high = n - 1;
+    int first = -1;
+
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        if(arr[mid] == target){
+            first = mid;
+            high = mid - 1; 
+        }
+        else if(arr[mid] > target){
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+
+    low = 0;
+    high = n - 1;
+    int last = -1;
+
+    while(low <= high){
+        int mid = low + (high - low)/2;
+        if(arr[mid] == target){
+            last = mid;
+            low = mid + 1; 
+        }
+        else if(arr[mid] > target){
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+
+    cout<<first<<" : "<<last;
+
+}
