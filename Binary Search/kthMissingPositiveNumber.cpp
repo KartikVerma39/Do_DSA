2f222ab12b38990b118d44777ecfa8c4aaaf0049
KartikVerma39 <kartikverma1074@gmail.com>
2025-12-17 11:35:00 +0530

    BinarySearch_DSA

---
+++ Binary Search/kthMissingPositiveNumber.cpp
@@ -0,0 +1,25 @@
+// 1539. Kth Missing Positive Number
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>arr = {2,3,4,7,11};
+    int k = 5;
+    int n = arr.size();
+    int low = 0;
+    int high = n - 1;
+
+    while(low<=high){
+        int mid = low + (high - low)/2;
+        int missing = arr[mid] - (mid + 1);
+        if(missing > k){
+            high = mid - 1;
+        }
+        else{
+            low = mid + 1;
+        }
+    }
+    cout<<low + k;
+    return 0;
+}
