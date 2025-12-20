0c05f37856478dd8b8928ab7b59e236c7d6337d4
KartikVerma39 <kartikverma1074@gmail.com>
2025-12-21 00:21:43 +0530

    BinarySearch_DSA

--- Binary Search/minimizeMaxDistanceToGasStation.cpp
+++ Binary Search/minimizeMaxDistanceToGasStation.cpp
@@ -47,3 +47,31 @@
 // Heap Approach
 
  
+
+#include<iostream>
+#include<queue>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>arr = {3, 6, 12, 19, 33};
+    int n = arr.size();
+    int k = 3;
+    vector<int>totalGas(n - 1 , 0);
+    priority_queue<pair<long double , int>>pq;
+
+    for(int i = 0 ; i < n - 1 ; i++){
+        pq.push({arr[i+1] - arr[i] , i});
+    }
+
+    for(int i = 1 ; i <= k ; i++){
+        auto tp = pq.top();
+        pq.pop();
+        int index = tp.second;
+        totalGas[index]++;
+        int distance = arr[index + 1] - arr[index];
+        int totalDis = distance/(long double)(totalGas[index] + 1);
+        pq.push({totalDis , index});
+    }
+
+    cout<<pq.top().first;
+}
